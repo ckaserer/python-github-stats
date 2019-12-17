45ed08f1c7efbf8a16ef6f08af129bd80f255b5f
Clemens Kaserer <clemens.kaserer@gmail.com>
2019-12-17 09:43:04 +0100

    add set -ex to docker-push-image.sh

--- docker-push-image.sh
+++ docker-push-image.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+set -ex
+
 if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
-    if [ "$TRAVIS_BRANCH" == "master" ]; then
-      echo "${DOCKER_PASS}" | docker login -u "${DOCKER_USER}" --password-stdin
-      docker push ${ORGANIZATION}/${REPO}
-    fi
-  else
-    echo -e "Skipping - pull requests can not utilize encrypted variables.\nFor further information see: https://docs.travis-ci.com/user/pull-requests/#pull-requests-and-security-restrictions"
+  if [ "${TRAVIS_BRANCH}" == "master" ]; then
+    echo "${DOCKER_PASS}" | docker login -u "${DOCKER_USER}" --password-stdin
+    docker push ${ORGANIZATION}/${REPO}
   fi
+else
+  echo -e "Skipping - pull requests can not utilize encrypted variables.\nFor further information see: https://docs.travis-ci.com/user/pull-requests/#pull-requests-and-security-restrictions"
+fi
