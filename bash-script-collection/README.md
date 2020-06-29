12871da99ddfce2b557cd500c0f7dec4f469e011
Clemens Kaserer <clemens.kaserer@gmail.com>
2020-06-29 09:39:12 +0200

    removed bash-script-collection

--- bash-script-collection/README.md
+++
@@ -1,6 +0,0 @@
-![GitHub](https://img.shields.io/github/license/ckaserer/bash-script-collection)
-![Maintenance](https://img.shields.io/maintenance/yes/2020)
-<br>
-<br>
-
-# bash-script-collection
--- bash-script-collection/functions/echo_stderr.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-function echo_stderr () { 
-  echo "$@" 1>&2; 
-}
-# readonly definition of a function throws an error if another function 
-# with the same name is defined a second time
-readonly -f echo_stderr
-[ "$?" -eq "0" ] || return $?
